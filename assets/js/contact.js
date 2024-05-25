30934095f1d43145478646a544e3fad75bb6c826
SwaeZi <stevenstefanov@icloud.com>
2024-05-25 10:39:58 +0200

    fix: minor improvement

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,15 +1,20 @@
 
   const form = document.getElementById("form");
   form.addEventListener("submit", function (event) {
-    event.preventDefault(); 
+    event.preventDefault();
 
     const nameInput = document.getElementById("name");
     const name = nameInput.value.trim();
 
     if (name.toLowerCase() === "ironhack") {
       alert("You cannot be Ironhack, because I am Ironhack.");
-      nameInput.value = ""; 
+      nameInput.value = "";
     } else {
-      this.submit();
+      const inputs = form.getElementsByTagName("input");
+      for (let input of inputs) {
+        input.value = "";
+      }
+      const textarea = form.getElementsByTagName("textarea")[0];
+      textarea.value = "";
     }
   });
